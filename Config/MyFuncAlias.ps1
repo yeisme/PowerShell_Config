d03d389715cd99abaa948821ca88de5c16f7550f
yesisme <yefun2004@gmail.com>
2025-03-09 14:55:43 +0800

    完善 readme

--- Config/MyFuncAlias.ps1
+++ Config/MyFuncAlias.ps1
@@ -13,7 +13,7 @@ function Invoke-PWSH {
     pwsh -NoProfile -NoExit @args
 }
 
-## git
+### git
 Set-Alias gc1 Invoke-GitCloneDepth1
 
 # eza
@@ -22,98 +22,9 @@ Set-Alias e Invoke-Eza
 # pwsh
 Set-Alias pd -Description "PowerShell Without Profile" Invoke-PWSH
 
-### Github Copilot alias
-function ghcs {
-    # Debug support provided by common PowerShell function parameters, which is natively aliased as -d or -db
-    # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_commonparameters?view=powershell-7.4#-debug
-    param(
-        [Parameter()]
-        [string]$Hostname,
+### neovim
 
-        [ValidateSet('gh', 'git', 'shell')]
-        [Alias('t')]
-        [String]$Target = 'shell',
-
-        [Parameter(Position = 0, ValueFromRemainingArguments)]
-        [string]$Prompt
-    )
-    begin {
-        # Create temporary file to store potential command user wants to execute when exiting
-        $executeCommandFile = New-TemporaryFile
-
-        # Store original value of GH_* environment variable
-        $envGhDebug = $Env:GH_DEBUG
-        $envGhHost = $Env:GH_HOST
-    }
-    process {
-        if ($PSBoundParameters['Debug']) {
-            $Env:GH_DEBUG = 'api'
-        }
-
-        $Env:GH_HOST = $Hostname
-
-        gh copilot suggest -t $Target -s "$executeCommandFile" $Prompt
-    }
-    end {
-        # Execute command contained within temporary file if it is not empty
-        if ($executeCommandFile.Length -gt 0) {
-            # Extract command to execute from temporary file
-            $executeCommand = (Get-Content -Path $executeCommandFile -Raw).Trim()
-
-            # Insert command into PowerShell up/down arrow key history
-            [Microsoft.PowerShell.PSConsoleReadLine]::AddToHistory($executeCommand)
-
-            # Insert command into PowerShell history
-            $now = Get-Date
-            $executeCommandHistoryItem = [PSCustomObject]@{
-                CommandLine        = $executeCommand
-                ExecutionStatus    = [Management.Automation.Runspaces.PipelineState]::NotStarted
-                StartExecutionTime = $now
-                EndExecutionTime   = $now.AddSeconds(1)
-            }
-            Add-History -InputObject $executeCommandHistoryItem
-
-            # Execute command
-            Write-Host "`n"
-            Invoke-Expression $executeCommand
-        }
-    }
-    clean {
-        # Clean up temporary file used to store potential command user wants to execute when exiting
-        Remove-Item -Path $executeCommandFile
-
-        # Restore GH_* environment variables to their original value
-        $Env:GH_DEBUG = $envGhDebug
-    }
-}
-
-function ghce {
-    # Debug support provided by common PowerShell function parameters, which is natively aliased as -d or -db
-    # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_commonparameters?view=powershell-7.4#-debug
-    param(
-        [Parameter()]
-        [string]$Hostname,
-
-        [Parameter(Position = 0, ValueFromRemainingArguments)]
-        [string[]]$Prompt
-    )
-    begin {
-        # Store original value of GH_* environment variables
-        $envGhDebug = $Env:GH_DEBUG
-        $envGhHost = $Env:GH_HOST
-    }
-    process {
-        if ($PSBoundParameters['Debug']) {
-            $Env:GH_DEBUG = 'api'
-        }
-
-        $Env:GH_HOST = $Hostname
-
-        gh copilot explain $Prompt
-    }
-    clean {
-        # Restore GH_* environment variables to their original value
-        $Env:GH_DEBUG = $envGhDebug
-        $Env:GH_HOST = $envGhHost
-    }
-}
+Set-Alias vi nvim
+Set-Alias vim nvim
+Set-Alias nd neovide
+Set-Alias nt nvim-qt
--- Config/Theme.ps1
+++ Config/Theme.ps1
@@ -6,17 +6,24 @@ function Set-MyPoshTheme {
     )
 
     oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/$Theme.omp.json" | Invoke-Expression
+
+    if ($Debug) {
+        Write-Host "Theme $Theme loaded" -ForegroundColor Green
+        Write-Host "oh-my-posh init pwsh --config  $env:POSH_THEMES_PATH/$Theme.omp.json" -ForegroundColor Green
+    }
 }
 
 
 # 示例：设置默认主题
-Set-MyPoshTheme -Theme "mojada"
+Set-MyPoshTheme -Theme "dracula"
 
 # 其他主题示例
 # Set-MyPoshTheme -Theme ""
+# Set-MyPoshTheme -Theme "mojada"
 # Set-MyPoshTheme -Theme "jandedobbeleer"
 # Set-MyPoshTheme -Theme "blueish"
 # Set-MyPoshTheme -Theme "atomic"
 # Set-MyPoshTheme -Theme "kali"
+# Set-MyPoshTheme -Theme powerlevel10k_rainbow
 
 # Get-PoshThemes # 查看所有主题
